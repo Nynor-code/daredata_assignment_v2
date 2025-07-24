3e8acf0c97a456384f067509b0f3a0d73e9aec76
Nynor <nynor@nfpires.com>
2025-07-24 18:01:18 +0100

    cleaning.py:25:0: W0102: Dangerous default value set() as argument (dangerous-default-value) correction

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -18,7 +18,7 @@ from life_expectancy.cleaning import (
 def test_clean_data(raw_life_expectancy_sample, pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
     cleaned = clean_data(raw_life_expectancy_sample,
-                         country_code=Region.PT
+                         export_country_code=Region.PT
                          )
     pd.testing.assert_frame_equal(
         cleaned.reset_index(drop=True),
@@ -29,7 +29,7 @@ def test_clean_data(raw_life_expectancy_sample, pt_life_expectancy_expected):
 @patch("pandas.DataFrame.to_csv")
 def test_save_data_mocked(mock_to_csv, pt_life_expectancy_expected):
     """Ensure save_data calls to_csv without writing to disk"""
-    save_data(pt_life_expectancy_expected, country_code="PT")
+    save_data(pt_life_expectancy_expected, export_country_code=Region.PT)
     mock_to_csv.assert_called_once()
     _, kwargs = mock_to_csv.call_args
     assert kwargs.get("index") is False
